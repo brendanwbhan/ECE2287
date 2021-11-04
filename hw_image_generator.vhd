d14d9b95eb8a296c8ecb6bfec22c6755b4160d7e
brendanwbhan <93613611+brendanwbhan@users.noreply.github.com>
2021-11-04 14:20:03 -0400

    Add files via upload
    Added a new display with two white dots in the middle with a black background. Currently figuring out how to show the seven-segment display on VGA.

--- hw_image_generator.vhd
+++ hw_image_generator.vhd
@@ -22,40 +22,60 @@
 
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 ENTITY hw_image_generator IS
-  GENERIC(
-    pixels_y :  INTEGER := 478;   --row that first color will persist until
-    pixels_x :  INTEGER := 600);  --column that first color will persist until
-  PORT(
-    disp_ena :  IN   STD_LOGIC;  --display enable ('1' = display time, '0' = blanking time)
-    row      :  IN   INTEGER;    --row pixel coordinate
-    column   :  IN   INTEGER;    --column pixel coordinate
-    red      :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --red magnitude output to DAC
-    green    :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --green magnitude output to DAC
-    blue     :  OUT  STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')); --blue magnitude output to DAC
+	GENERIC(
+		p_left	:	INTEGER := 0; --860
+		p_right	:	INTEGER := 0; --1060
+		p_top		:	INTEGER := 0; --440
+		p_bottom	:	INTEGER := 0); --640
+	PORT(
+		s0	:	IN STD_LOGIC;
+		s1	:	IN	STD_LOGIC;
+		s2	:	IN STD_LOGIC;
+		s3	:	IN STD_LOGIC;
+		disp_ena	:	IN		STD_LOGIC;	--display enable ('1' = display time, '0' = blanking time)
+		row		:	IN		INTEGER;		--row pixel coordinate
+		column	:	IN		INTEGER;		--column pixel coordinate
+		red		:	OUT	STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --red magnitude output to DAC
+		green		:	OUT	STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0');  --green magnitude output to DAC
+		blue		:	OUT	STD_LOGIC_VECTOR(7 DOWNTO 0) := (OTHERS => '0')); --blue magnitude output to DAC
 END hw_image_generator;
 
 ARCHITECTURE behavior OF hw_image_generator IS
 BEGIN
-  PROCESS(disp_ena, row, column)
-  BEGIN
-
-    IF(disp_ena = '1') THEN        --display time
-      IF(row < pixels_y AND column < pixels_x) THEN
-        red <= (OTHERS => '0');
-        green  <= (OTHERS => '0');
-        blue <= (OTHERS => '1');
-      ELSE
-        red <= (OTHERS => '1');
-        green  <= (OTHERS => '1');
-        blue <= (OTHERS => '0');
-      END IF;
-    ELSE                           --blanking time
-      red <= (OTHERS => '0');
-      green <= (OTHERS => '0');
-      blue <= (OTHERS => '0');
-    END IF;
-  
-  END PROCESS;
+	PROCESS(s0, s1, s2, s3, disp_ena, row, column)
+	BEGIN
+		IF(disp_ena = '1') THEN	--display time
+			-- Display two dots in the middle
+			IF(row > 935 AND row < 985 AND column > 440 AND column < 490) THEN --top dot
+				red <= (OTHERS => '1');
+				green	<= (OTHERS => '1');
+				blue <= (OTHERS => '1');
+			ELSIF (row > 935 AND row < 985 AND column > 590 AND column < 640) THEN --bottom dot
+				red <= (OTHERS => '1');
+				green	<= (OTHERS => '1');
+				blue <= (OTHERS => '1');
+				
+			-- Display first digit
+			ELSIF(s0 = '1' AND row > 5 AND row < 105 AND column > 430 AND column < 440) THEN --top signal
+				red <= (OTHERS => '1');
+				green	<= (OTHERS => '1');
+				blue <= (OTHERS => '1');
+			ELSIF(s0 = '1' AND row > 115 AND row < 125 AND column > 445 AND column < 545) THEN
+				red <= (OTHERS => '1');
+				green	<= (OTHERS => '1');
+				blue <= (OTHERS => '1');
+			ELSE
+				red <= (OTHERS => '0');
+				green	<= (OTHERS => '0');
+				blue <= (OTHERS => '0');
+			END IF;	
+		ELSE								--blanking time
+			red <= (OTHERS => '0');
+			green <= (OTHERS => '0');
+			blue <= (OTHERS => '0');
+		END IF;
+	END PROCESS;
 END behavior;
