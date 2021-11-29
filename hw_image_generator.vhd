d7ad16a9c200781f4732c083af9e9bababf4fca0
brendanwbhan <93613611+brendanwbhan@users.noreply.github.com>
2021-11-29 15:00:20 -0500

    Add files via upload
    Final version with a title

--- hw_image_generator.vhd
+++ hw_image_generator.vhd
@@ -52,11 +52,19 @@ architecture behavior of hw_image_generator is
 	-- signal sec_digits : std_logic_vector (7 downto 0);
 	-- signal min_digits : std_logic_vector (7 downto 0);
 
+	shared variable a1 : std_logic_vector(0 to 11) := "111110011110";
+	shared variable b1 : std_logic_vector(0 to 11) := "010111111001";
+	shared variable c1 : std_logic_vector(0 to 11) := "110101111001";
+	shared variable d1 : std_logic_vector(0 to 11) := "100101100010";
+	shared variable e1 : std_logic_vector(0 to 11) := "000111010011";
+	shared variable f1 : std_logic_vector(0 to 11) := "100111010011";
+	shared variable g1 : std_logic_vector(0 to 11) := "100010010001";
 	shared variable digit_pixel_range : display_coor;
 	shared variable extra : integer := 0;
 
 	function seven_segment_display(a, b, c, d, e, f, g	: std_logic:='0';
-											offset_x, offset_y		: integer := 0)
+											offset_x, offset_y		: integer := 0;
+											h, w : integer)
 											return display_coor is
 		--The format of the output array will be the coordinate of a, b, c,..., g respectively.
 		-- Coordinate will be in this order (x1,x2,y1, y2)
@@ -65,45 +73,45 @@ architecture behavior of hw_image_generator is
 		begin
 			if (a = '1') then
 				coordinate(0) (0) := (offset_x);
-				coordinate(0) (1) := (offset_x) + 200;
+				coordinate(0) (1) := (offset_x) + w;
 				coordinate(0) (2) := (offset_y);
 				coordinate(0) (3) := (offset_y) + thickness;
 			end if;
 			if (b = '1') then
-				coordinate(1) (0) := (offset_x) + 200;
-				coordinate(1) (1) := (offset_x) + 200 + thickness;
+				coordinate(1) (0) := (offset_x) + w;
+				coordinate(1) (1) := (offset_x) + w + thickness;
 				coordinate(1) (2) := (offset_y);
-				coordinate(1) (3) := (offset_y) + 175;
+				coordinate(1) (3) := (offset_y) + h;
 			end if;
 			if (c = '1') then
-				coordinate(2) (0) := (offset_x) + 200;
-				coordinate(2) (1) := (offset_x) + 200 + thickness;
-				coordinate(2) (2) := (offset_y) + 180;
-				coordinate(2) (3) := (offset_y) + 180 * 2;
+				coordinate(2) (0) := (offset_x) + w;
+				coordinate(2) (1) := (offset_x) + w + thickness;
+				coordinate(2) (2) := (offset_y) + h;
+				coordinate(2) (3) := (offset_y) + h * 2;
 			end if;
 			if (d = '1') then
 				coordinate(3) (0) := (offset_x);
-				coordinate(3) (1) := (offset_x) + 200;
-				coordinate(3) (2) := (offset_y) + 180 * 2;
-				coordinate(3) (3) := (offset_y) + 180 * 2 + thickness;
+				coordinate(3) (1) := (offset_x) + w;
+				coordinate(3) (2) := (offset_y) + h * 2;
+				coordinate(3) (3) := (offset_y) + h * 2 + thickness;
 			end if;
 			if (e = '1') then
 				coordinate(4) (0) := (offset_x);
 				coordinate(4) (1) := (offset_x) + thickness;
-				coordinate(4) (2) := (offset_y) + 180;
-				coordinate(4) (3) := (offset_y) + 180 * 2;
+				coordinate(4) (2) := (offset_y) + h;
+				coordinate(4) (3) := (offset_y) + h * 2;
 			end if;
 			if (f = '1') then
 				coordinate(5) (0) := (offset_x);
 				coordinate(5) (1) := (offset_x) + thickness;
 				coordinate(5) (2) := (offset_y);
-				coordinate(5) (3) := (offset_y) + 180;
+				coordinate(5) (3) := (offset_y) + h;
 			end if;
 			if (g = '1') then
 				coordinate(6) (0) := (offset_x);
-				coordinate(6) (1) := (offset_x) + 200;
-				coordinate(6) (2) := (offset_y) + 180;
-				coordinate(6) (3) := (offset_y) + 180 + thickness;
+				coordinate(6) (1) := (offset_x) + w;
+				coordinate(6) (2) := (offset_y) + h;
+				coordinate(6) (3) := (offset_y) + h + thickness;
 			end if;
 
 			return coordinate;
@@ -120,6 +128,9 @@ architecture behavior of hw_image_generator is
 		-- Create boxes containing the time number
 		IF(disp_ena = '1') THEN	--display time
 
+			
+				
+				
 			-- Display Minutes
 			IF(column > 20 + 130 AND column < 260 + 130 AND row > p_topDigit AND row < p_bottomDigit) THEN --top dot
 				red <= (OTHERS => '1');
@@ -170,6 +181,49 @@ architecture behavior of hw_image_generator is
 			-- 	green	<= (OTHERS => '1');
 			-- 	blue <= (OTHERS => '1');
 			END IF;
+			
+			-------------------------------------------------------------------------------------------
+			
+			for i in 0 to 11 loop
+				digit_pixel_range := seven_segment_display(a1(i), b1(i), c1(i), d1(i), e1(i), f1(i), g1(i), 400 + 95*i, 50, 50,70);
+
+				if (column > digit_pixel_range(0)(0) and column < digit_pixel_range(0)(1) and row > digit_pixel_range(0)(2) and row < digit_pixel_range(0)(3)) then
+					red <= (others => '0');
+					green <= (others => '1');
+					blue <= (others => '0');
+				end if;
+				if (column > digit_pixel_range(1)(0) and column < digit_pixel_range(1)(1) and row > digit_pixel_range(1)(2) and row < digit_pixel_range(1)(3)) then
+						red <= (others => '0');
+						green <= (others => '1');
+						blue <= (others => '0');
+				end if;
+				if (column > digit_pixel_range(2)(0) and column < digit_pixel_range(2)(1) and row > digit_pixel_range(2)(2) and row < digit_pixel_range(2)(3)) then
+					red <= (others => '0');
+					green <= (others => '1');
+					blue <= (others => '0');
+				end if;
+				if (column > digit_pixel_range(3)(0) and column < digit_pixel_range(3)(1) and row > digit_pixel_range(3)(2) and row < digit_pixel_range(3)(3)) then
+					red <= (others => '0');
+					green <= (others => '1');
+					blue <= (others => '0');
+				end if;
+				if (column > digit_pixel_range(4)(0) and column < digit_pixel_range(4)(1) and row > digit_pixel_range(4)(2) and row < digit_pixel_range(4)(3)) then
+					red <= (others => '0');
+					green <= (others => '1');
+					blue <= (others => '0');
+				end if;
+				if (column > digit_pixel_range(5)(0) and column < digit_pixel_range(5)(1) and row > digit_pixel_range(5)(2) and row < digit_pixel_range(5)(3)) then
+					red <= (others => '0');
+					green <= (others => '1');
+					blue <= (others => '0');
+				end if;
+				if (column > digit_pixel_range(6)(0) and column < digit_pixel_range(6)(1) and row > digit_pixel_range(6)(2) and row < digit_pixel_range(6)(3)) then
+					red <= (others => '0');
+					green <= (others => '1');
+					blue <= (others => '0');
+				end if;
+			end loop;
+			--------------------------------------------------------------
 
 			for i in 0 to 5 loop
 				if (i >= 4) then
@@ -179,7 +233,7 @@ architecture behavior of hw_image_generator is
 				else
 					extra := 0;
 				end if;
-				digit_pixel_range := seven_segment_display(a(i), b(i), c(i), d(i), e(i), f(i), g(i), 30 + 260*i + 40*extra + 130, 350);
+				digit_pixel_range := seven_segment_display(a(i), b(i), c(i), d(i), e(i), f(i), g(i), 30 + 260*i + 40*extra + 130, 350, 180, 200);
 
 				if (column > digit_pixel_range(0)(0) and column < digit_pixel_range(0)(1) and row > digit_pixel_range(0)(2) and row < digit_pixel_range(0)(3)) then
 					red <= (others => '0');
